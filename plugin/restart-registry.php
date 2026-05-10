b54c29d5074dd5177ecc75d62d04be9a5a74ffd6
Andrew Neher <andrew.neher1@gmail.com>
2026-05-09 20:23:53 -0500

    savepoint

--- plugin/restart-registry.php
+++ plugin/restart-registry.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Restart Registry
  * Plugin URI:        http://the-restart.co/restart-registry-uri/
  * Description:       Provides a gift registry custom post type, add item function, and associated features.
- * Version: 1.0.20
+ * Version: 		  1.0.20
  * Author:            Andrew Neher
  * Author URI:        http://the-restart.co/
  * License:           GPL-2.0+
