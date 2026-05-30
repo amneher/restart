58fe3a7e7724046db5688d3ffc680f9d995a23f2
Andrew Neher <andrew.neher1@gmail.com>
2026-05-30 15:53:38 -0500

    savepoint

--- scripts/txt_to_gh_issue.py
+++ scripts/txt_to_gh_issue.py
@@ -3,38 +3,49 @@
 import subprocess
 import sys
 from pathlib import Path
+from typing import Union
 
 BASE_DIR = Path(__file__).parent
 
+def create_issue(title: str, body: str):
+    res = subprocess.run(
+        [
+            "gh",
+            "issue",
+            "create",
+            "--assignee",
+            "@me",
+            "--title",
+            title,
+            "--body",
+            body,
+        ]
+    )
+    print(res)
 
-def main(filename: str):
-    file = BASE_DIR / filename
-    with open(file, "r") as f:
-        content = f.readlines()
-        for line in content:
-            parts = line.split("|")
-            title = parts[0].strip()
-            body = parts[1].strip()
-            res = subprocess.run(
-                [
-                    "gh",
-                    "issue",
-                    "create",
-                    "--assignee",
-                    "@me",
-                    "--title",
-                    title,
-                    "--body",
-                    body,
-                ]
-            )
-            print(res)
+def main(input: Union[str, list[str]]):
+    if isinstance(input, list):
+        title = input[0]
+        body = input[1]
+        create_issue(title, body)
+
+    else:
+        filename = input
+        file = BASE_DIR / filename
+        with open(file, "r") as f:
+            content = f.readlines()
+            for line in content:
+                parts = line.split("|")
+                title = parts[0].strip()
+                body = parts[1].strip()
+                create_issue(title, body)
 
 
 if __name__ == "__main__":
+    # print(sys.argv)
     args = sys.argv[1:]
     if args:
-        main(args[0])
+        main(args)
     else:
         user_file = input("Enter the filename (default: issues.txt): ")
         if not user_file:
